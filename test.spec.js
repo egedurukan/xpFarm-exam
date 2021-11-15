3e24d13e69555c135c25175a383a8e5584bcb682
EgeDurukan <ege.durukan1@ibm.com>
2021-11-15 15:02:49 +0100

    first test on playerMove - RED

--- test.spec.js
+++ test.spec.js
@@ -5,3 +5,12 @@ const assert = chai.assert;
 chai.should();
 
 eval(fs.readFileSync('./tictactoe.js')+'');
+
+newGame();
+
+describe('test playerMove()', () => {
+    it('should return existing element from moves list',() => {
+        const movelist = ["0,0", "0,1","0,2","1,0","1,1","1,2","2,0","2,1","2,2"];
+        movelist.includes(playerMove()).should.equal(true);
+    });
+});
