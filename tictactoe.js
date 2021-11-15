5aec7c1869f1322a6049499992a7e5c67c4e56e4
EgeDurukan <ege.durukan1@ibm.com>
2021-11-15 15:12:41 +0100

    added vertical checker - GREEN

--- tictactoe.js
+++ tictactoe.js
@@ -18,3 +18,27 @@ function inputValidator(input){
 
     return true;
 }
+
+function validateCollection (collection) {
+    let winner = '';
+    collection.forEach(row => {
+        if(row.every(value => value === "X")) {
+            winner = "Player X won!";
+        } else if(row.every(value => value === "O")) {
+            winner = "Player O won!";
+        }
+    });
+    return winner;
+}
+
+function checkVertical (board) {
+    let verticalLineColl = [];
+    for(i = 0; i < board.length; i++){
+        let verticalLine = [];
+        for(j=0; j<board[i].length; j++){
+            verticalLine.push(board[j][i])
+        }
+        verticalLineColl.push(verticalLine);
+    }
+    return validateCollection(verticalLineColl);
+}
